de71b186bdb0453b8a585089639b6bf23b9a66c1
Fletcher17b <oiramgrillol.2@gmail.com>
2024-09-20 08:11:20 -0600

    Adelanto

---
+++ ejercicio 3/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ejercicio_3.Models;
+
+namespace ejercicio_3
+{
+    public partial class Form1 : Form
+    {
+        public static int[] list1 = new int[4] { 0, 1, 1, 2 };
+        public static int[] list2 = new int[4] { 0, 5, 0, 0 };
+        public static int[] list3 = new int[4] { 2, 0, 3, 3 };
+        public int[][] lists2 = new int[][] { list1, list2, list3 };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int row = 0;
+        int col = 0;
+        int[,] matrix1 = new int[2,2];
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                float return_value = Helper.TestMethod(matrix1);
+                label1.Text = return_value.ToString();
+            }
+            catch (Exception ex) { }
+        }
+    }
+}
---
+++ ejercicio 3/Models/Helper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_3.Models
+{
+    internal class Helper
+    {
+        
+        public static float TestMethod(int[][] matrix)
+        {
+            float sum = 0;
+            for (int column = 0; column < 4; column++)
+                for (int row = 0; row < 3; row++)
+                {
+                    if (matrix[row][column] != 0)
+                        sum += matrix[row][column];
+                    else
+                        break;
+                }
+            return sum;
+        }
+
+        public static float TestMethod(int[,] matrix)
+        {
+            float sum = 0;
+            for (int column = 0; column < 4; column++)
+                for (int row = 0; row < 3; row++)
+                {
+                    if (matrix[row,column] != 0)
+                        sum += matrix[row,column];
+                    else
+                        break;
+                }
+            return sum;
+        }
+    }
+}
